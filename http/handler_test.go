9f0e31d212044fb6ea04ed561e7b42b4fc3ad09a
Dmitry <stalin-777@mail.ru>
2021-09-29 14:05:10 +0600

    Added test case

--- http/handler_test.go
+++ http/handler_test.go
@@ -15,8 +15,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var workplaceJSON = `{"data":{"id":1,"hostname":"localhost","ip":"127.0.0.1","username":"user1"},"success":true}
+var (
+	workplaceJSON = `{"data":{"id":1,"hostname":"localhost","ip":"127.0.0.1","username":"user1"},"success":true}
 `
+	workplacesJSON = `{"data":[{"id":1,"hostname":"localhost","ip":"127.0.0.1","username":"user1"}],"success":true}
+`
+)
 
 func TestHandler_FindWorkplace(t *testing.T) {
 
@@ -43,7 +47,12 @@ func TestHandler_FindWorkplace(t *testing.T) {
 	wps := &mock.WorkplaceService{}
 	wps.FindWorkplaceFn = func(id int) (*aoj.Workplace, error) {
 		if id == 1 {
-			return &aoj.Workplace{1, "localhost", net.ParseIP("127.0.0.1"), "user1"}, nil
+			return &aoj.Workplace{
+				ID:       1,
+				Hostname: "localhost",
+				IP:       net.ParseIP("127.0.0.1"),
+				Username: "user1",
+			}, nil
 		}
 		return nil, errors.New("Some error")
 	}
@@ -52,9 +61,54 @@ func TestHandler_FindWorkplace(t *testing.T) {
 	// Assertions
 	if assert.NoError(t, h.FindWorkplace(c)) {
 		if !wps.FindWorkplaceInvoked {
-			t.Fatal("expected Workplace() to be invoked")
+			t.Fatal("expected FindWorkplace() to be invoked")
 		}
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, workplaceJSON, rec.Body.String())
+	}
+}
+
+func TestHandler_FindWorkplaces(t *testing.T) {
+
+	// Setup
+	err := logger.InitZapLogger(
+		"../logs",
+		"log.log",
+		10,
+		0,
+		0,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/workplaces")
+	c.SetParamNames("id")
+	c.SetParamValues("1")
+
+	wps := &mock.WorkplaceService{}
+	wps.FindWorkplacesFn = func() ([]*aoj.Workplace, error) {
+		return []*aoj.Workplace{
+			{
+				ID:       1,
+				Hostname: "localhost",
+				IP:       net.ParseIP("127.0.0.1"),
+				Username: "user1",
+			},
+		}, nil
+	}
+	h := &Handler{WorkplaceService: wps}
+
+	// Assertions
+	if assert.NoError(t, h.FindWorkplaces(c)) {
+		if !wps.FindWorkplacesInvoked {
+			t.Fatal("expected FindWorkplaces() to be invoked")
+		}
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, workplacesJSON, rec.Body.String())
 	}
 }
